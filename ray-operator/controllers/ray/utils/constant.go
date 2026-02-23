143d4d43dee4dc0a2e3a9dee6edcb05050f0962d
Pat O'Connor <paoconno@redhat.com>
2026-02-23 13:46:04 +0000

    feat(RHOAIENG-47878): Contribute NetworkPolicy feat upstream
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- ray-operator/controllers/ray/utils/constant.go
+++ ray-operator/controllers/ray/utils/constant.go
@@ -437,6 +437,7 @@ const (
 	CreatedRoleBinding        K8sEventType = "CreatedRoleBinding"
 	FailedToCreateRoleBinding K8sEventType = "FailedToCreateRoleBinding"
 
+<<<<<<< HEAD
 	// mTLS event list
 	MTLSPKIReady               K8sEventType = "MTLSPKIReady"
 	MTLSCertsNotReady          K8sEventType = "MTLSCertsNotReady"
@@ -447,4 +448,10 @@ const (
 	MTLSFailedToReconcile      K8sEventType = "MTLSFailedToReconcile"
 	MTLSCertificatesUpdated    K8sEventType = "MTLSCertificatesUpdated"
 	MTLSFailedToCleanupSecrets K8sEventType = "MTLSFailedToCleanupSecrets"
+=======
+	// NetworkPolicy event list
+	CreatedNetworkPolicy        K8sEventType = "CreatedNetworkPolicy"
+	DeletedNetworkPolicy        K8sEventType = "DeletedNetworkPolicy"
+	FailedToCreateNetworkPolicy K8sEventType = "FailedToCreateNetworkPolicy"
+>>>>>>> 7c081772 (feat(RHOAIENG-47878): Contribute NetworkPolicy feat upstream)
 )
